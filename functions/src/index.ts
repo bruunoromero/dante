1ba437998d31bc9d2695cad02ca026844b378cab
brunoromero <brunoromero172@hotmail.com>
2019-03-09 23:20:47 -0300

    sets up firebase admin and move goals to its own folder

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,45 +1,10 @@
-import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-export const createGoalAction = functions.firestore
-  .document("goals/{goalId}/actions/{actionId}")
-  .onCreate(snap => {
-    const goal = snap.ref.parent.parent as FirebaseFirestore.DocumentReference;
+const serviceAccount = require("../serviceAccountKey.json");
 
-    goal.firestore.runTransaction(async (tx: FirebaseFirestore.Transaction) => {
-      const goalSnap = await tx.get(goal);
-      const data = goalSnap.data();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://dante-d7735.firebaseio.com"
+});
 
-      if (!data) return;
-
-      const numberOfActions = (data.numberOfActions || 0) + 1;
-      await tx.update(goal, { numberOfActions });
-    });
-  });
-
-export const toogleGoalActionDone = functions.firestore
-  .document("goals/{goalId}/actions/{actionId}")
-  .onUpdate(change => {
-    if (change.after && change.before) {
-      const newValue = change.after.data();
-      const oldValue = change.before.data();
-
-      if (!newValue || !oldValue) return;
-      if (newValue.isDone === oldValue.isDone) return;
-
-      const goal = change.after.ref.parent
-        .parent as FirebaseFirestore.DocumentReference;
-
-      goal.firestore.runTransaction(
-        async (tx: FirebaseFirestore.Transaction) => {
-          const goalSnap = await tx.get(goal);
-          const data = goalSnap.data();
-
-          if (!data) return;
-
-          const numberOfDoneActions =
-            (data.numberOfDoneActions || 0) + (newValue.isDone ? 1 : -1);
-          await tx.update(goal, { numberOfDoneActions });
-        }
-      );
-    }
-  });
+export * from "./goals";
